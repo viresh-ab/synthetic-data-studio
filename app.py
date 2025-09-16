335f4a6a35453a4d3a887183a8ff66ff8c5d6d5c
Viresh AB <74718448+viresh-ab@users.noreply.github.com>
2025-09-16 16:46:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ hide_st_style = """
     #MainMenu {visibility: hidden;}
     footer {visibility: hidden;}
     header {visibility: hidden;}
+    ._profilePreview_gzau3_63{display:none;}
     </style>
 """
 st.markdown(hide_st_style, unsafe_allow_html=True)
